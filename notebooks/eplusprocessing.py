abc4b99905e69747a54985640a9edcccea56b6b1
cpatdowling <cpatdowling@gmail.com>
2019-06-19 17:06:28 -0700

    adding notebooks

---
+++ notebooks/eplusprocessing.py
@@ -0,0 +1,228 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import datetime
+from statsmodels.tsa import stattools
+from sklearn import preprocessing
+import random
+import copy
+import scipy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+
+#features to ignore"
+ignore = ["Performance Curve Input Variable 1 Value [] !Each Call", "Performance Curve Output Value [] !Each Call"]
+
+#def parse_line()
+
+def read_data_dictionary(lines):
+    data_dict = {}
+    for line in lines:
+        l = line.strip()
+        if l == "End of Data Dictionary":
+            break
+        else:
+            tokens = l.split(",")
+            if int(tokens[0]) == 2:
+                data_dict[int(tokens[0])] = tokens[1:]
+                #parse time dictionary line
+            elif int(tokens[0]) > 6 and int(tokens[1]) == 1:
+                if tokens[-1] in ignore:
+                    pass
+                elif len(tokens) == 4:
+                    data_dict[int(tokens[0])] = tokens[-2] + tokens[-1]
+                    
+                else:
+                    #parse value dictionary line
+                    data_dict[int(tokens[0])] = tokens[-1]
+            else:
+                pass
+    return(data_dict)
+
+def parse_time_line(line_str):
+    #2,8,Day of Simulation[],Month[],Day of Month[],DST Indicator[1=yes 0=no],Hour[],StartMinute[],EndMinute[],DayType
+    #2, 1, 1, 1, 0, 1, 0.00, 2.00, Holiday
+    #only want hourly times, multiple minute 2's, assuming got correct
+    tokens = line_str.strip().split(",")
+    minute=float(tokens[-3])
+    hour=int(tokens[-4])
+    day=int(tokens[-6])
+    month=int(tokens[-7])
+    return(datetime.datetime(2017, month, day, hour-1, 0, 0)) #2017 so that the 1st is a Sunday + not leapyear
+    #return(str(month) + "/" + str(day) + " " + str(hour) + ":00")
+     
+def parse_lines(lines, feature_inds):
+    data_values = {}
+    data_values[2] = []
+    for i in feature_inds:
+        data_values[i] = []
+    
+    start_i = lines.index("End of Data Dictionary\n")
+    for line in lines[start_i+1:-2]:
+        tokens = line.strip().split(",")
+        if int(tokens[0]) == 2:
+            if float(tokens[-3]) == 0.00 and float(tokens[-2]) == 60.00:
+                t = parse_time_line(line)
+                data_values[2].append(t)
+        elif int(tokens[0]) in feature_inds:
+            data_values[int(tokens[0])].append(float(tokens[-1]))
+        else:
+            pass
+    return(data_values)
+    #return hourly list for all feature vars, plus hourly list with times to check sorting
+    
+def get_holidays(lines):
+    holidays = []
+    weekends = []
+    all_dates = []
+    
+    start_i = lines.index("End of Data Dictionary\n")
+    for line in lines[start_i+1:-2]:
+        tokens = line.strip().split(",")
+        if tokens[-1] == "Holiday":
+            if float(tokens[-3]) == 0.00 and float(tokens[-2]) == 60.00:
+                t = parse_time_line(line)
+                holidays.append(t)
+        elif int(tokens[0]) == 2:
+            if float(tokens[-3]) == 0.00 and float(tokens[-2]) == 60.00:
+                t = parse_time_line(line)
+                all_dates.append(t)
+                if t.weekday() == 6 or t.weekday() == 0:
+                    weekends.append(t)
+    return(all_dates, weekends, holidays)
+                      
+def norm_array(arr):
+    return(preprocessing.minmax_scale(arr, axis=1))
+
+def read_total_data_array(filepath, dep_var_names, ind_var_names, zone_temp_inds, first_diff=False, no_weekend_holiday=False):
+    with open(filepath) as d:
+        lines = d.readlines()
+        lines.pop(0)
+        data_dict = read_data_dictionary(lines)
+        data = parse_lines(lines, data_dict)
+        all_dates, weekends, holidays = get_holidays(lines)
+        ind_map = {}
+        for key, value in data_dict.items():
+            if type(value) == list:
+                value = value[-1]
+            ind_map[value] = key
+
+    X = np.zeros((len(dep_var_names) + len(zone_temp_inds), 8760))
+    U = np.zeros((len(ind_var_names), 8760))
+
+    for i in range(len(ind_var_names)):
+        item = ind_var_names[i]
+        ind = ind_map[item]
+        if item == 'DayType':
+            lis = data[ind]
+            out = []
+            for val in lis:
+                out.append(val.weekday())
+            arr = np.array(out)
+        else:
+            arr = np.array(data[ind])
+        U[i,:] = arr
+
+    for j in range(len(dep_var_names)):
+        item = dep_var_names[j]
+        ind = ind_map[item]
+        arr = np.array(data[ind])
+        X[j,:] = arr
+
+    for j in range(len(zone_temp_inds)):
+        ind = zone_temp_inds[j]
+        arr = np.array(data[ind])
+        X[j+len(dep_var_names),:] = arr
+        
+    if no_weekend_holiday == True:
+        weekend_i = []
+        holiday_i = []
+        for j in range(len(all_dates)):
+            if all_dates[j] in weekends:
+                weekend_i.append(j)
+            if all_dates[j] in holidays:
+                holiday_i.append(j)
+                
+        del_i = np.array(list(set(holiday_i + weekend_i)))
+        X = np.delete(X, del_i, axis=1)
+        U = np.delete(U, del_i, axis=1)
+    
+    if first_diff == True:
+        X = np.diff(X, n=1, axis=1)
+        U = np.diff(U, n=1, axis=1)
+
+    Z = np.vstack((X, U))
+
+    return(Z, X, U)
+
+def minibatch_X_Y_arrays(X_arr, Y_arr, batchsize):    
+    #list of training, target pair tuples
+    remainder = X_arr.shape[1] % batchsize
+    diff = batchsize - remainder
+    tail_X = X_arr[:,-diff:] 
+    tail_Y = Y_arr[:,-diff:]
+    out_X = [ X_arr[:,i*batchsize:(i+1)*batchsize] for i in range(int(float(X_arr.shape[1])/float(batchsize))) ]
+    out_Y = [ Y_arr[:,i*batchsize:(i+1)*batchsize] for i in range(int(float(Y_arr.shape[1])/float(batchsize)))]
+    out_X = out_X + [tail_X]
+    out_Y = out_Y + [tail_Y]
+    return(out_X, out_Y)
+
+def split_train_val(Z, X, U, order=1, shuff=True, split_prop=0.8):
+    all_pairs = []
+    for i in range(Z.shape[1] - order):  #not 8760 for first-diff
+        all_pairs.append((Z[:,i:i+order-1].flatten(), Z[0:-U.shape[0],i+order]))
+
+    if shuff == True:
+        random.shuffle(all_pairs) 
+    else:
+        pass
+
+    train_pairs = all_pairs[0:int(split_prop*len(all_pairs))]
+    val_pairs = all_pairs[-int((1.0-split_prop)*len(all_pairs)):]
+    
+    #normalize to training data
+    X_train = []
+    Y_train = []
+
+    for item in train_pairs:
+        X_train.append(item[0])
+        Y_train.append(item[1])
+
+    X_val = []
+    Y_val = []
+    for item in val_pairs:
+        X_val.append(item[0])
+        Y_val.append(item[1])
+
+    X_train = np.asarray(X_train).T
+    Y_train = np.asarray(Y_train).T
+
+    X_val = np.asarray(X_val).T
+    Y_val = np.asarray(Y_val).T
+    
+    return(X_train, Y_train, X_val, Y_val)
+
+def normalize_data_set(X, Y, X_minmax_vals):
+    X_norm = copy.copy(X)
+    Y_norm = copy.copy(Y)
+    
+    for row in range(X.shape[0]):
+        mx = np.max(X_minmax_vals[row,:])
+        mn = np.min(X_minmax_vals[row,:])
+
+        X_norm[row,:] = (1.0/(mx - mn))*(X[row,:] - mn)
+
+        if row < Y.shape[0]:
+            Y_norm[row,:] = (1.0/(mx - mn))*(Y[row,:] - mn)
+            
+    return(X_norm, Y_norm)
+
+def polynomial_kernel_mat(X, poly_degree):
+    out = X
+    for d in range(2,poly_degree+1):
+        out = np.vstack((out, np.power(X, d)))
+    return(out)
